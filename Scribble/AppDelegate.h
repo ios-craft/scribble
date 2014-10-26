64153f62c42f0004b3007e92e80059b561a7bd28
Tomek Cejner <tomek.cejner@gmail.com>
2014-10-26 15:17:56 +0100

    Initial Commit

---
+++ Scribble/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Scribble
+//
+//  Created by Tomek Cejner on 26/10/14.
+//  Copyright (c) 2014 Tomek Cejner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Scribble/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Scribble
+//
+//  Created by Tomek Cejner on 26/10/14.
+//  Copyright (c) 2014 Tomek Cejner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

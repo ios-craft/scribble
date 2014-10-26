7ab49ca6e704f589ca831344c2af1c00a2360793
Tomek Cejner <tomek.cejner@gmail.com>
2014-10-26 15:39:51 +0100

    Empty scribble view

---
+++ Scribble/ScribbleView.m
@@ -0,0 +1,21 @@
+//
+//  ScribbleView.m
+//  Scribble
+//
+//  Created by Tomek Cejner on 26/10/14.
+//  Copyright (c) 2014 Tomek Cejner. All rights reserved.
+//
+
+#import "ScribbleView.h"
+
+@implementation ScribbleView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end

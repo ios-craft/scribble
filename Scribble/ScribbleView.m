05ca883a412d7ffd5dddf98936cfe8a115a92949
Tomek Cejner <tomek.cejner@gmail.com>
2014-10-26 21:00:51 +0100

    Naive scribble finished

--- Scribble/ScribbleView.m
+++ Scribble/ScribbleView.m
@@ -8,14 +8,107 @@
 
 #import "ScribbleView.h"
 
-@implementation ScribbleView
+static float dotSize = 16.0;
 
-/*
+@implementation ScribbleView
+{
+    NSMutableArray *points;
+    NSMutableArray *currentStroke;
+
+}
+
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self initializeScribble];
+    }
+
+    return self;
+}
+
+- (id)initWithCoder:(NSCoder *)coder {
+    self = [super initWithCoder:coder];
+    if (self) {
+        [self initializeScribble];
+    }
+    return self;
+}
+
+- (void)initializeScribble {
+    NSLog(@"Initializing View");
+    points = [NSMutableArray arrayWithCapacity:500];
+}
+
+
+
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect {
     // Drawing code
+    CGContextRef context = UIGraphicsGetCurrentContext();
+
+    for (NSMutableArray *stroke in points) {
+        if (stroke.count == 1) {
+            CGPoint p = [stroke[0] CGPointValue];
+            [self drawDotAtPoint:p inContext:context];
+        } else {
+            NSEnumerator *enumerator = [stroke objectEnumerator];
+            NSValue *head = [enumerator nextObject];
+            CGPoint headPoint = [head CGPointValue];
+            CGContextMoveToPoint(context, headPoint.x, headPoint.y);
+
+            NSValue *node;
+            while (node = [enumerator nextObject]) {
+                CGPoint point = [node CGPointValue];
+                CGContextAddLineToPoint(context, point.x, point.y);
+            }
+
+            CGContextSetLineWidth(context, dotSize);
+            CGContextSetLineCap(context, kCGLineCapRound);
+            CGContextStrokePath(context);
+        }
+    }
+
+
 }
-*/
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    CGPoint loc = [[touches anyObject] locationInView:self];
+    NSLog(@"TOUCH %.0f %.0f", loc.x, loc.y);
+    currentStroke = [NSMutableArray array];
+    [self finalizePath];
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+    CGPoint loc = [[touches anyObject] locationInView:self];
+    NSLog(@"END   %.0f %.0f", loc.x, loc.y);
+    [self addPathPointAt:loc];
+    [self setNeedsDisplay];
+}
+
+- (void)finalizePath {
+    [points addObject:currentStroke];
+}
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
+    CGPoint loc = [[touches anyObject] locationInView:self];
+    NSLog(@"MOVE  %.0f %.0f", loc.x, loc.y);
+
+    [self addPathPointAt:loc];
+    [self setNeedsDisplay];
+}
+
+- (void)addPathPointAt:(CGPoint)point {
+    [currentStroke addObject:[NSValue valueWithCGPoint:point]];
+}
+
+- (void)drawDotAtPoint:(CGPoint)loc inContext:(CGContextRef)context {
+    CGRect frame = CGRectMake(loc.x - dotSize / 2.0f, loc.y - dotSize / 2.0f, dotSize, dotSize);
+
+    CGContextSetFillColorWithColor (context,[[UIColor blackColor] CGColor]);
+    CGContextFillEllipseInRect(context, frame);
+}
+
 
 @end

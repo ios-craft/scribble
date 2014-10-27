f704f967551a873c5e8c956c8cc678da0417676e
Tomek Cejner <tomek.cejner@gmail.com>
2014-10-27 07:34:27 +0100

    Basic scribble drawing.

---
+++ Scribble/ScribbleView.h
@@ -0,0 +1,13 @@
+//
+//  ScribbleView.h
+//  Scribble
+//
+//  Created by Tomek Cejner on 26/10/14.
+//  Copyright (c) 2014 Tomek Cejner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ScribbleView : UIView
+
+@end
